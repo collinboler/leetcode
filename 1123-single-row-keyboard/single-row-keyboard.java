164aa59d7a12bb5f7b60e84a5cec060dd3e1d66a
collinboler <155112701+collinboler@users.noreply.github.com>
2025-05-04 19:47:04 -0400

    Time: 4 ms (96.71%) | Memory: 42.6 MB (23.68%) - LeetSync

--- 1123-single-row-keyboard/single-row-keyboard.java
+++ 1123-single-row-keyboard/single-row-keyboard.java
@@ -1,15 +1,21 @@
-class Solution {
-    public int calculateTime(String keyboard, String word) {
-        int count = 0;
-        char[] wordArr = word.toCharArray();
-
-        for (int i  = 0; i < wordArr.length; i++) {
-            if (i == 0) count += keyboard.indexOf(wordArr[i]);
-            else {
-                count += (Math.abs(keyboard.indexOf(wordArr[(i-1)]) - keyboard.indexOf(wordArr[(i)])));
-            }
+class Solution 
+{
+    public int calculateTime(String keyboard, String word) 
+    {
+        int[] index = new int[26];
+        for (int i = 0; i < keyboard.length(); ++i)
+        {
+            index[keyboard.charAt(i) - 'a'] = i;
         }
-        return count;
-
+        
+        int sum = 0;
+        int last = 0;
+        for (char c : word.toCharArray())
+        {
+            sum += Math.abs(index[c - 'a'] - last);
+            last = index[c - 'a'];
+        }
+        
+        return sum;
     }
 }
