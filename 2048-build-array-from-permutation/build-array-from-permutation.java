44c6753f7085ea50603ede229d457fc04ff4759f
collinboler <155112701+collinboler@users.noreply.github.com>
2025-05-06 12:40:14 -0400

    Time: 1 ms (98.72%) | Memory: 45.9 MB (7.75%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] solution = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            solution[i] = nums[nums[i]];
+        }
+        return solution;
+
+    }
+}
