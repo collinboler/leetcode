8b06c39911bcbefbc34dab67d8483aa77937c3dd
collinboler <155112701+collinboler@users.noreply.github.com>
2025-08-02 00:36:15 -0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (88.57%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -5,24 +5,18 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         bigList = []
-        for i in range(numRows): 
-            if (i == 0):
-                bigList.append([1]) 
-            elif (i == 1):
-                bigList.append([1,1])     
+        for i in range(numRows):
+            if i == 0:
+                bigList.append([1])
+            elif i == 1:
+                bigList.append([1, 1])
             else:
+                row = []
+                row.append(1)
                 prev = bigList[-1]
-                row = [1]
                 for j in range(1, len(prev)):
-                    row.append(prev[j - 1] + prev[j])
+                    row.append(prev[j-1] + prev[j])
                 row.append(1)
                 bigList.append(row)
         return bigList
-                 
-           
-        
-        
-            
 
-       
-        
