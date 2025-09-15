00823ead7795753200a4e37e7057782cf975968c
collinboler <155112701+collinboler@users.noreply.github.com>
2025-09-14 23:27:39 -0400

    Time: 6 ms (16.01%) | Memory: 41.4 MB (100.00%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -8,30 +8,6 @@ class Solution {
             set.add(c);
         }
 
-        // int count = 0;
-       
-        // String[] words = text.split(" ");
-
-        // // Print each word
-        // for (String word : words) {
-        //     int i = word.length();
-        //     for (char c : word.toCharArray()) {
-        //         i--;
-        //         if (set.contains(c)) {
-        //             System.out.println("hey" + word);
-        //             break;
-        //         }
-        //         else if (i == 0) {
-        //             count++;
-        //         }
-                
-        //     }
-        // }
-
-
-        //  String[] words = text.split(" ");
-
-        // Print each word
         int count = 0;
        
         for (int i = 0; i < text.length(); i++) {
