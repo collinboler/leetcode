a4d80049a77025b1a67a5cb657990e07a8418c96
collinboler <155112701+collinboler@users.noreply.github.com>
2025-09-14 23:26:30 -0400

    Time: 6 ms (16.01%) | Memory: 41.8 MB (96.29%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -8,24 +8,42 @@ class Solution {
             set.add(c);
         }
 
-        int count = 0;
+        // int count = 0;
        
-        String[] words = text.split(" ");
+        // String[] words = text.split(" ");
+
+        // // Print each word
+        // for (String word : words) {
+        //     int i = word.length();
+        //     for (char c : word.toCharArray()) {
+        //         i--;
+        //         if (set.contains(c)) {
+        //             System.out.println("hey" + word);
+        //             break;
+        //         }
+        //         else if (i == 0) {
+        //             count++;
+        //         }
+                
+        //     }
+        // }
+
+
+        //  String[] words = text.split(" ");
 
         // Print each word
-        for (String word : words) {
-            int i = word.length();
-            for (char c : word.toCharArray()) {
-                i--;
-                if (set.contains(c)) {
-                    System.out.println("hey" + word);
-                    break;
-                }
-                else if (i == 0) {
-                    count++;
-                }
-                
+        int count = 0;
+       
+        for (int i = 0; i < text.length(); i++) {
+            boolean bad = false;
+
+            while (i < text.length() && text.charAt(i)!= ' ') {
+                if (set.contains(text.charAt(i))) bad = true;
+                i++;
             }
+            // i++;
+            if (!bad) count++;
+        
         }
         return count;
     }
