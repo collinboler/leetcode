718d12ae6dbad58995df817b3ce13ba27a909c28
collinboler <155112701+collinboler@users.noreply.github.com>
2025-09-17 18:18:34 -0400

    Time: 6 ms (71.52%) | Memory: 64.7 MB (18.88%) - LeetSync

---
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        if (((k * 2) + 1) > nums.length) {
+            int[] solution = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                solution[i] = -1;
+            }
+            return solution;
+        }
+
+
+        int radius = (k * 2) + 1;
+
+        int[] solution = new int[nums.length];
+        long curr = 0;
+        for (int i = 0; i < k; i++) {
+            solution[i] = - 1;
+            curr += nums[i];
+        }
+        for (int i = k; i < radius; i++) {
+            curr += nums[i];
+        }
+
+        solution[k] = (int)(curr / radius);
+        int left = 0;
+   
+        int right = radius;
+        System.out.println(curr);
+
+        for (int middle = k + 1; middle <= nums.length - k; middle++) {
+            if (right > nums.length - 1) break;
+
+            curr += (nums[right] - nums[left]);
+            solution[middle] = (int)(curr / radius);
+            left++;
+            right++;
+        }
+        for (int i = nums.length - k; i < nums.length; i++) {
+            solution[i] = - 1;
+        }
+        return solution;
+    }
+}
