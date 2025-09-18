5983699dd2cd9f5c3117f03f38dc0d70d53d04e4
collinboler <155112701+collinboler@users.noreply.github.com>
2025-09-17 21:36:41 -0400

    Time: 13 ms (12.59%) | Memory: 42.7 MB (32.11%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        String balloon = "balloon";
+        for (char c: balloon.toCharArray()) {
+            map.put(c, 0);
+        }
+        for (char c : text.toCharArray()) {
+            if (map.containsKey(c)) map.put(c, map.get(c) + 1);
+        }
+        
+        for (char i : map.keySet()) {
+            System.out.println(i + " " + map.get(i));
+ 
+        }
+        int min = Integer.MAX_VALUE;
+        for (char c : map.keySet()) {
+            if (c != 'o' || c != 'l' )  min = Math.min(map.get(c), min);
+           
+        }
+        
+        return Math.min(min, Math.min(map.get('o') / 2, map.get('l') / 2));
+    } 
+}
