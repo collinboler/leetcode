8f9d3d15b00136e8314a59babdeb9c7162532c41
collinboler <155112701+collinboler@users.noreply.github.com>
2025-09-29 00:32:58 -0400

    Time: 3 ms (48.13%) | Memory: 42.5 MB (37.01%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s.length() == 0) return s;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty() && stack.peek() == Character.toUpperCase(c) && c != Character.toUpperCase(c)) {
+                stack.pop();
+            }
+            else if (!stack.isEmpty() && stack.peek() == Character.toLowerCase(c) && c != Character.toLowerCase(c)) {
+                stack.pop();
+              
+            }
+            else stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
+
+        
+        
+    
+}
+}
