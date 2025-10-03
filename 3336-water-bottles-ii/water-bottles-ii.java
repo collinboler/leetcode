189fc89ca47c3bb463d903131a119db4a347f3c0
collinboler <155112701+collinboler@users.noreply.github.com>
2025-10-03 00:45:58 -0400

    Time: 0 ms (100.00%) | Memory: 40.8 MB (68.42%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.java
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public int maxBottlesDrunk(int numBottles, int numExchange) {
-        int drunk = numBottles; // total bottles drunk
-        int empty = numBottles; // empty bottles after drinking initial ones
+        int count = numBottles; // drink all
+        int empty = count; // num empty
         
-        while (empty >= numExchange) {
-            // Exchange once
-            empty -= numExchange;  // use up required empty bottles
-            numExchange++;         // cost increases by 1
-            drunk++;               // drink the new full bottle
-            empty++;               // that bottle becomes empty
+        while (empty >= numExchange ) {
+            count++;
+            empty -=  numExchange - 1;
+            numExchange++;
         }
-        
-        return drunk;
+          
+        return count;
     }
 }
