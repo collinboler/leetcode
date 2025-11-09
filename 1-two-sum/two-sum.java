80ecebb155461743ede3abf0da3018de875d2160
collinboler <155112701+collinboler@users.noreply.github.com>
2025-11-09 01:23:05 -0500

    Time: 2 ms (99.11%) | Memory: 47.3 MB (5.22%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -6,12 +6,13 @@ class Solution {
        for (int i = 0; i < nums.length; i++) {
         int complement = target - nums[i]; 
 
-       if (map.containsKey(complement)) {
+         if (map.containsKey(complement)) {
         return new int[] {map.get(complement), i};
-       } 
+        } 
 
         else map.put(nums[i], i);
        }
        return null;
+ 
     }
 }
