5929c908e19d50786c27c7822e096d18b776e36a
collinboler <155112701+collinboler@users.noreply.github.com>
2025-11-14 15:41:24 -0500

    Time: 2 ms (99.14%) | Memory: 47.1 MB (5.22%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -14,7 +14,7 @@ class Solution {
                 result[1] = i;
                 return result;
             }
-            map.put(nums[i], i );
+            map.put(nums[i], i);
         }
          return result;
         }
