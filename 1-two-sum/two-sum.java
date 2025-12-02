8df932e620bac706491dcf09bf13a5c56948c182
collinboler <155112701+collinboler@users.noreply.github.com>
2025-12-02 18:12:28 -0500

    Time: 2 ms (99.15%) | Memory: 47.1 MB (18.23%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,26 +1,20 @@
-import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // declare hashmap
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] result = new int[2];
-        // pass through nums
-       
-       for (int i = 0; i < nums.length; i++) {
-            // see if target - nums[i] is in db, if yes return both, if no add nums[i], i to db
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                result[0] = map.get(complement);
-                result[1] = i;
-                return result;
-            }
-            map.put(nums[i], i);
-        }
-         return result;
-        }
-        // return empty int[] if not
-        
-        // done :)
-    }
+     // declare hashmap
     
+    HashMap<Integer, Integer> map = new HashMap<>();
+     // one pass through array
 
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) return new int[] {i, map.get(complement)};
+        else map.put(nums[i], i);
+    }
+
+    return new int[]{};
+     // check for complement, if found return both indicies
+
+
+     // return empty array if none found after pass 
+    }
+}
