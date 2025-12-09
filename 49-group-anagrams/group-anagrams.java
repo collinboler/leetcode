eaab0b427ee2b7d6ff17e620745cfc92de024482
collinboler <155112701+collinboler@users.noreply.github.com>
2025-12-08 23:37:11 -0500

    Time: 9 ms (35.79%) | Memory: 49.7 MB (35.37%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,10 +1,7 @@
 class Solution {
     private String alpha(String s) {
-        // System.out.println("og string: " + s);
         char[] charray = s.toCharArray();
         Arrays.sort(charray);
-        // System.out.println("changed string: " + new String(charray));
-
         return new String(charray);
     }
     public List<List<String>> groupAnagrams(String[] strs) {
