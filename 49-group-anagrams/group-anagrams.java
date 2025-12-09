98bca95f14445389c249d6d653f5aa516c6b4f71
collinboler <155112701+collinboler@users.noreply.github.com>
2025-12-08 23:36:03 -0500

    Time: 129 ms (5.06%) | Memory: 50.5 MB (9.62%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,30 @@
+class Solution {
+    private String alpha(String s) {
+        System.out.println("og string: " + s);
+        char[] charray = s.toCharArray();
+        Arrays.sort(charray);
+        System.out.println("changed string: " + new String(charray));
+
+        return new String(charray);
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            if (map.containsKey(alpha(s))) {
+                // check for duplicate
+                map.get(alpha(s)).add(s); // add to the list in the hashmap
+            }
+            else { // populate new list
+                List<String> list = new ArrayList<String>();
+                list.add(s);
+                map.put(alpha(s), list);
+            }
+        }
+        List<List<String>> result = new ArrayList<List<String>>();
+        for (String key : map.keySet()) {
+            result.add(map.get(key));
+        }
+        return result;
+    }
+}
