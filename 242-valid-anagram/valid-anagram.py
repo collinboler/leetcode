a62344b8a43da46fc1f35c9d2b5e1b2d604986fe
collinboler <155112701+collinboler@users.noreply.github.com>
2025-12-11 13:48:00 -0500

    Time: 12 ms (89.80%) | Memory: 12.7 MB (67.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,24 +5,49 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
         if len(s) != len(t):
             return False
-        m = {}
-        for i in range(len(s)):
-            if s[i] not in m:
-                m[s[i]] = 1
+
+        chars = {}
+        for i in s:
+            if i in chars:
+                chars[i] += 1
             else:
-                m[s[i]] = m.get(s[i]) + 1
-        
-        for j in range(len(t)):
-            if t[j] not in m:
+                chars[i] = 1
+        for x in t:
+            if x in chars:
+                chars[x] -= 1
+            else:
                 return False
-            elif m.get(t[j]) > 1:
-                m[t[j]] = m.get(t[j]) - 1
-            else:
-                del m[t[j]]
-        
+        for c in chars:
+            if chars[c] != 0:
+                return False
         return True
+        
+        # if len(s) != len(t):
+        #     return False
+        # m = {}
+        # for i in range(len(s)):
+        #     if s[i] not in m:
+        #         m[s[i]] = 1
+        #     else:
+        #         m[s[i]] = m.get(s[i]) + 1
+        
+        # for j in range(len(t)):
+        #     if t[j] not in m:
+        #         return False
+        #     elif m.get(t[j]) > 1:
+        #         m[t[j]] = m.get(t[j]) - 1
+        #     else:
+        #         del m[t[j]]
+        
+        # return True
 
 
 
