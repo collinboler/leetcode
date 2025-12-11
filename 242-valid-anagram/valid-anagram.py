f968ffdf94f5353a3a8b3e9b84886321c77dc84b
collinboler <155112701+collinboler@users.noreply.github.com>
2025-12-11 13:53:34 -0500

    Time: 10 ms (95.42%) | Memory: 12.7 MB (67.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,31 +5,28 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
         if len(s) != len(t):
             return False
-
+        
         chars = {}
+        
         for i in s:
             if i in chars:
                 chars[i] += 1
             else:
                 chars[i] = 1
-        for x in t:
-            if x in chars:
-                chars[x] -= 1
+
+        for j in t:
+            if j in chars:
+                chars[j] -= 1
             else:
                 return False
+                       
         for c in chars:
             if chars[c] != 0:
                 return False
-        return True
         
+        return True
         # if len(s) != len(t):
         #     return False
         # m = {}
@@ -47,7 +44,7 @@ class Solution(object):
         #     else:
         #         del m[t[j]]
         
-        # return True
+        # # return True
 
 
 
