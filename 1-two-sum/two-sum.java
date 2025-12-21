4c2320e2b695bf11ad06b248de449963a9367ccf
collinboler <155112701+collinboler@users.noreply.github.com>
2025-12-21 15:59:09 -0500

    Time: 2 ms (99.10%) | Memory: 47.1 MB (23.27%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+       
+    HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             int comp = target - nums[i];
@@ -8,7 +9,6 @@ class Solution {
             else map.put(nums[i], i);
         }
         return new int[]{};
-   
 
     }
 }
