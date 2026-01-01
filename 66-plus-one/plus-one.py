8ad4e288af2765a5c1475055b0e3340fea5a07df
collinboler <155112701+collinboler@users.noreply.github.com>
2026-01-01 18:15:11 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (11.64%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -15,14 +15,12 @@ class Solution(object):
         i = last
         while(digits[i] == 9 and i >= 0):
             nineCount += 1
-            print(digits[i])
             digits[i] = 0
             i -= 1
         if nineCount == len(digits):
             digits.insert(0, 1)
             return digits
-        print(digits)
-        print(nineCount)
+       
         digits[i] += 1
         return digits
                 
