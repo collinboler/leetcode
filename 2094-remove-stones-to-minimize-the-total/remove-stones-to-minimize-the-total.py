e63d565595b511e2b757c4eed6abb00aaee60c8e
collinboler <155112701+collinboler@users.noreply.github.com>
2026-01-01 19:05:28 -0500

    Time: 850 ms (10.00%) | Memory: 31.7 MB (9.76%) - LeetSync

---
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
@@ -0,0 +1,29 @@
+import heapq
+
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        h = []
+        heapq.heapify(h)
+
+        for pile in piles:
+            print(-pile)
+            heapq.heappush(h, -pile)
+        
+        print(h)
+        result = 0
+        for i in range(k):
+            val = heapq.heappop(h)
+            # result += math.floor(val / 2)
+            heapq.heappush(h, math.floor(val / 2))
+
+            # print(f"iteration  {i + 1} out of {k}; result = {result}, val = {val}")
+        
+        # print(h)
+        for x in h:
+            result += -x
+            
+        
+        return result
+
+
+        
