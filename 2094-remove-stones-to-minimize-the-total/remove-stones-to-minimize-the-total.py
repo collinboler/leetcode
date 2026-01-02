81f3cd404d0afed44d8fdbecb7b3d63ba0b5ae2f
collinboler <155112701+collinboler@users.noreply.github.com>
2026-01-01 20:06:00 -0500

    Time: 596 ms (45.04%) | Memory: 30.5 MB (14.15%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
@@ -12,12 +12,9 @@ class Solution:
         result = 0
         for i in range(k):
             val = heapq.heappop(h)
-            # result += math.floor(val / 2)
             heapq.heappush(h, math.floor(val / 2))
 
-            # print(f"iteration  {i + 1} out of {k}; result = {result}, val = {val}")
-        
-        # print(h)
+           
         for x in h:
             result += -x
             
