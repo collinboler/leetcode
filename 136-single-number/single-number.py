7eb23a911e81ceeafd5fe3a81a9f81fc4da83339
collinboler <155112701+collinboler@users.noreply.github.com>
2026-01-02 14:56:01 -0500

    Time: 3 ms (65.34%) | Memory: 19.5 MB (75.94%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashset = set()
+
+        for num in nums:
+            if num not in hashset:
+                hashset.add(num)
+            else:
+                hashset.discard(num)
+        
+        return hashset.pop()
+
+        
