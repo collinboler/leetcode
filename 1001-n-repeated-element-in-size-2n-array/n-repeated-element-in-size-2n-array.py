f75025275a3928271aef47012679ef5585603348
collinboler <155112701+collinboler@users.noreply.github.com>
2026-01-01 21:27:59 -0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (85.16%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -1,30 +1,17 @@
-class Solution:
-    def repeatedNTimes(self, nums: List[int]) -> int:
-        # simple one pass solution
-        maxNum = -1
-        maxy = 0
-        mappy = {}
+class Solution(object):
+    def repeatedNTimes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        setty = set()
 
         for num in nums:
-            if num in mappy:
-                mappy[num] += 1
-                if (mappy[num] > maxy):
-                    maxNum = num
-                    maxy = mappy[num]
-            else: 
-                mappy[num] = 1
-        
-        return maxNum
+            if num in setty:
+                return num
             
-                   
-            
-            
-
-
-
+            else:
+                setty.add(num)
         
-
-
-
-        
+        return -1
         
