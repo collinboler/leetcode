49e10a79bfecb3060f3a231ca797cafedcfcf38e
collinboler <155112701+collinboler@users.noreply.github.com>
2026-02-23 03:22:15 -0500

    Time: 0 ms (100.00%) | Memory: 13.5 MB (18.18%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,42 +1,26 @@
-class Solution:
-    # (LeetDuck) Start loop and dictionary initialization here.
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         map = {}
 
         for i in range(len(nums)):
+
             complement = target - nums[i]
+            # (LeetDuck) Use map[complement] to get the correct index.
             if complement in map:
-                return [map[complement], i]
-            else:
-                map[nums[i]] = i
+                return [i, map[complement]]
+            map[nums[i]] = i
         
-        return
-
-        # confident with logic, not worried about edge cases
-        
-        
-         
-        
-        
-        
-        
-
-        
-        
+        return []
+       
+       
 
 
-
-        
-        
-        
-        
-        
-        
-        
-
-
-        
         
        
         
-       
+      
