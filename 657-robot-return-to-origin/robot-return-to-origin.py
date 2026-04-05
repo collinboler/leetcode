f43092dfc86069efefa7db7fbf9650958a1ca05d
collinboler <155112701+collinboler@users.noreply.github.com>
2026-04-05 16:59:54 -0400

    Time: 15 ms (60.35%) | Memory: 19 MB (99.84%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,23 @@
+class Solution:
+    # (LeetDuck) Start your implementation here.
+    def judgeCircle(self, moves: str) -> bool:
+        L = 0
+        R = 0
+        U = 0
+        D = 0
+
+        for move in moves:
+            if move == 'U':
+                U += 1
+            elif move == 'D':
+                D += 1
+            elif move == 'R':
+                R += 1
+            elif move == 'L':
+                L += 1
+        
+        if L != R or U != D:
+            return False
+        return True
+        
+        
