ad97e14d588439857f25dc7b1f09dd5bfa1ee228
collinboler <155112701+collinboler@users.noreply.github.com>
2026-04-10 04:40:02 -0400

    Time: 0 ms (100.00%) | Memory: 13.1 MB (59.05%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def twoSum(self, nums, target):
         """
+        # (LeetDuck) Please add your implementation logic here.
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
