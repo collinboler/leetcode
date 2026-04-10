c1d93ac0ac99f7cf9d39281dc183e85140416755
collinboler <155112701+collinboler@users.noreply.github.com>
2026-04-10 04:33:50 -0400

    Time: 0 ms (100.00%) | Memory: 13.1 MB (73.51%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,17 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        map = {}
-
-        for i in range(len(nums)):
-
-            complement = target - nums[i]
-            # (LeetDuck) Use map[complement] to get the correct index.
-            if complement in map:
-                return [i, map[complement]]
-            map[nums[i]] = i
-        
+        prev_map = {}  # val : index
+    
+        for i, n in enumerate(nums):
+            diff = target - n
+            # If the required complement is already in our map, return the pair
+            if diff in prev_map:
+                return [prev_map[diff], i]
+            # Otherwise, add current number and its index to the map
+            prev_map[n] = i
         return []
+       
        
        
 
