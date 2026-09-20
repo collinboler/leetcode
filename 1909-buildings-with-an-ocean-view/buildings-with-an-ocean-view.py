608e636e1834fe4b7cb5b188f71fab3b758c887a
collinboler <155112701+collinboler@users.noreply.github.com>
2026-09-20 13:31:34 -0400

    Time: 24 ms (80.33%) | Memory: 27.2 MB (86.89%) - LeetSync

---
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution(object):
+    def findBuildings(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: List[int]
+        """
+        solution = deque()
+        
+        tallest = 0
+        i = len(heights) - 1
+        while i > -1:
+            if heights[i] > tallest:
+                tallest = heights[i]
+                solution.appendleft(i)
+            i -= 1
+        
+        return list(solution)
+
+
+
+        
