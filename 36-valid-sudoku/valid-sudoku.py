35fa16fd4b5f3d2e142956ee7d71006ec394c0a2
collinboler <155112701+collinboler@users.noreply.github.com>
2026-09-20 16:24:19 -0400

    Time: 15 ms (9.74%) | Memory: 12.4 MB (24.86%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,82 @@
+class Solution(object):
+    def whichGrid(self, row, col):
+        if row <= 2:
+            if col <= 2:
+                return 0
+            elif col <= 5:
+                return 1
+            else:
+                return 2
+        if row <= 5:
+            if col <= 2:
+                return 3
+            elif col <= 5:
+                return 4
+            else:
+                return 5
+        else:
+            if col <= 2:
+                return 6
+            elif col <= 5:
+                return 7
+            else:
+                return 8
+        return 0
+    
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        columns = {}
+        i = 0
+        while i < 9:
+            columns[i] = set()
+            i += 1
+
+        grids = {}
+        i = 0
+        while i < 9:
+            grids[i] = set()
+            i += 1
+        
+        # go through rows
+        for rowIndex, rowVal in enumerate(board):
+            rowSeen = set()
+            # go through col in row x
+            for columnIndex, columnVal in enumerate(rowVal):
+                if columnVal != ".":
+                    # column check
+                    if columnVal in columns[columnIndex]:
+                        print("column case:")
+                        print(columns)
+                        return False
+                    columns[columnIndex].add(columnVal)
+
+                    # grid check
+                    gridIndex = self.whichGrid(rowIndex, columnIndex)
+                    if columnVal in grids[gridIndex]:
+                        print(gridIndex)
+                        print(columnVal)
+                        print(rowIndex)
+                        print(columnIndex)
+                        print("grid case:")
+                        print(grids)
+                        return False
+                    grids[gridIndex].add(columnVal)
+
+                    # row check
+                    if columnVal in rowSeen:
+                        print("row case: {rowSeen}")
+                        return False
+                    else:
+                        rowSeen.add(columnVal)
+        
+        return True
+                
+                    
+            
+
+
+
+        
