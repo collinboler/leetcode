051f2aff9707039c50f36092ef7a339b4211fcfa
collinboler <155112701+collinboler@users.noreply.github.com>
2026-09-19 22:47:02 -0400

    Time: 2 ms (58.38%) | Memory: 13 MB (69.08%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,27 +1,16 @@
 class Solution(object):
     def twoSum(self, nums, target):
         """
-        # (LeetDuck) Please add your implementation logic here.
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        prev_map = {}  # val : index
-    
-        for i, n in enumerate(nums):
-            diff = target - n
-            # If the required complement is already in our map, return the pair
-            if diff in prev_map:
-                return [prev_map[diff], i]
-            # Otherwise, add current number and its index to the map
-            prev_map[n] = i
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return i, seen[complement]
+            else:
+                seen[num] = i
         return []
-       
-       
-       
 
-
-        
-       
-        
-      
