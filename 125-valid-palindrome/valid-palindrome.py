d32935d999a45cfcb59dc157821f3d1f88160757
collinboler <155112701+collinboler@users.noreply.github.com>
2026-09-20 20:18:11 -0400

    Time: 180 ms (16.50%) | Memory: 14.9 MB (6.15%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        letters = []
+        # handle stupid edge case:
+        
+        for c in s:
+            if c.isalpha() or c.isdigit():
+                letters.append(c.lower())
+        
+        print(letters)
+        if len(letters) % 2 == 0:
+            right = len(letters) - 1
+            for left in range(len(letters) / 2):
+
+                if letters[right] != letters[left]:
+                    return False
+                right -= 1
+            return True
+        
+        else:
+            right = len(letters) - 1
+            for left in range(len(letters) // 2):
+                if letters[right] != letters[left]:
+                    return False
+                right -= 1
+            return True
