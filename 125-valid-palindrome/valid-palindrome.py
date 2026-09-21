e00e77a3d0865c10ad98f45440c4784e6f19fcf6
collinboler <155112701+collinboler@users.noreply.github.com>
2026-09-21 10:16:38 -0400

    Time: 418 ms (5.01%) | Memory: 12.7 MB (63.03%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,23 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        letters = []        
-        for c in s:
-            if c.isalnum():
-                letters.append(c.lower())
-
-        if len(letters) % 2 == 0:
-            right = len(letters) - 1
-            for left in range(len(letters) / 2):
-                if letters[right] != letters[left]:
-                    return False
-                right -= 1
-            return True
         
-        else:
-            right = len(letters) - 1
-            for left in range(len(letters) // 2):
-                if letters[right] != letters[left]:
-                    return False
+        # remove invalid characters        
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            print("left: %s, right: %s" % (left,right))
+            print("left: %s, right: %s" % (s[left],s[right]))
+
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
                 right -= 1
-            return True
+                continue
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
+
+        
+        
